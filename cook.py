2890216e6c764192d31e6d57d1ba8b8266fa15ef
Martin Mensik <mmensik@gmail.com>
2012-07-15 17:22:58 +0200

    Options in command line added

--- cook.py
+++ cook.py
@@ -6,7 +6,7 @@
 #  Main file for the utility
 #
 
-from cmd2 import Cmd
+from cmd2 import Cmd, make_option, options, Cmd2TestCase
 from general.cmdUtils import query_yn
 
 import ofoam
@@ -17,10 +17,12 @@ class CookCmd(Cmd):
     
     problem = ofoam.Problem()
     
-    def do_load(self, arg):
-        'Loads all oopenFoam structures in the current directory'
+    @options([make_option('-v', '--verbose', action="store_true", help="verbose")])
+    def do_load(self, arg, opts=None):
+        '''Loads all oopenFoam structures in the current directory
+        '''
         self.problem.loadBoundaries()
-        if arg == 'v':
+        if opts.verbose:
             print self.problem.boundaries
     
     def do_ask(self, arg):
